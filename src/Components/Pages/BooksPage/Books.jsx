9bb368055cda63f1a4e485dd991c42dbdab109cc
Md Emon <mdshajdulhaqueemon@gmail.com>
2024-10-16 15:50:41 +0600

    update data

--- src/Components/Pages/BooksPage/Books.jsx
+++ src/Components/Pages/BooksPage/Books.jsx
@@ -44,8 +44,10 @@ const BookDetails = () => {
 
   return (
     <div>
-      <h1>Book Details</h1>
-      <div className="book-list grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-3">
+      <h1 className="text-center font-bold text-2xl text-[blue] mb-4">
+        Book Details
+      </h1>
+      <div className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
         {books.map((book) => (
           <div key={book.id} className="bookcard-style mx-auto">
             {book.formats["image/jpeg"] && (
